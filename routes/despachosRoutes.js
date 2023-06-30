81066c194fae326cc6760edf50719e78ba1b218a
guille alonso <guilloalonsot@gmail.com>
2023-06-30 16:27:50 -0300

    agregando verifyRoleSupervisor

--- routes/despachosRoutes.js
+++ routes/despachosRoutes.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
-const verifyRole = require("../middlewares/verifyRole");
 const auth = require("../middlewares/auth");
 const { check } = require("express-validator");
 const validateFields = require("../middlewares/validateFields");
 const { agregarDespacho, getDespachos } = require("../controllers/despachosControllers");
+const verifyRoleSupervisor = require("../middlewares/verifyRoleSupervisor");
 
 const router = Router();
 
-router.post("/alta",agregarDespacho)
-router.get("/listar/:id?",getDespachos);
+router.post("/alta",auth,verifyRoleSupervisor,agregarDespacho)
+router.get("/listar/:id?",auth,verifyRoleSupervisor,getDespachos);
 
 module.exports = router;
